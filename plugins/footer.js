9de84452ff80e1f8a2fa1eeba4edc74a3b750373
哈2哈哈哈3 <2430778099@qq.com>
2024-08-02 12:08:36 +0800

    修改页脚

--- plugins/footer.js
+++ plugins/footer.js
@@ -2,8 +2,8 @@
     "use strict";
     window.$docsify = window.$docsify || {}, window.$docsify.plugins = (window.$docsify.plugins || []).concat((function(o, f) {
         f.config.footer;
-        let e = f.config.footer && f.config.footer.copy ? f.config.footer.copy : "<span>&copy;TATyRealms Wiki 2021</span>",
-            t = f.config.footer && f.config.footer.auth ? f.config.footer.auth : '<span>本网址基于 <a href="https://github.com/docsifyjs/docsify" target="_blank" rel="noreferrer" rel="noopener">docsify</a>.</span>',
+        let e = f.config.footer && f.config.footer.copy ? f.config.footer.copy : "<span>TATyRealms &copy; 2021-<script type=\"text/javascript\">document.write(new Date().getFullYear());</script></span>",
+            t = f.config.footer && f.config.footer.auth ? f.config.footer.auth : '<span>本网站基于 <a href="https://github.com/docsifyjs/docsify" target="_blank" rel="noreferrer" rel="noopener">docsify</a>. By TATyTEAM</span>',
             c = f.config.footer && f.config.footer.style ? `style="${f.config.footer.style}"` : "",
             n = f.config.footer && f.config.footer.class ? `class="${f.config.footer.class}"` : "";
         var i = `${f.config.footer&&f.config.footer.pre?`${f.config.footer.pre}`:""}<footer ${c} ${n}>${e} ${t}</footer>`;
